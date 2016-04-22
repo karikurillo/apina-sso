3ff780aa06acd133acdde5198c6d6f07d2c63e32
karikurillo <kari.kurillo@gmail.com>
2016-04-22 15:26:21 +0300

    Refactoring authentication service to read realm from path

--- apina-sso/apina-sso-core/src/main/java/com/apina/sso/core/services/v1/RestAuthenticationV1.java
+++ apina-sso/apina-sso-core/src/main/java/com/apina/sso/core/services/v1/RestAuthenticationV1.java
@@ -1,8 +1,12 @@
 package com.apina.sso.core.services.v1;
 
+import com.apina.sso.core.realm.RealmManager;
+import com.apina.sso.core.security.SecurityManager;
 import com.apina.sso.core.services.v1.pojos.RestResponseAuthentication;
 import com.apina.sso.core.session.SessionManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.beanvalidation.OptionalValidatorFactoryBean;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,9 @@ public class RestAuthenticationV1 {
     @Autowired
     private SessionManager sessionManager;
 
+    @Autowired
+    private SecurityManager securityManager;
+
     @RequestMapping(value = "/v1/test", method = RequestMethod.GET)
     public String message() {
         return "Hello World!";
@@ -23,9 +30,26 @@ public class RestAuthenticationV1 {
 
     @RequestMapping(value = "/v1/authenticate", method = RequestMethod.GET)
     public RestResponseAuthentication authenticate() {
-        RestResponseAuthentication response = new RestResponseAuthentication(sessionManager.generateToken(), "All is good");
-        response.addAttribute("custom-attribute-1", "custom-value-1");
-        return response;
+        return authenticateUser(RealmManager.ROOT_REALM, "", "");
     }
 
+    @RequestMapping(value = "/v1/{realm}/authenticate", method = RequestMethod.GET)
+    public RestResponseAuthentication authenticate(@PathVariable(value = "realm") String realm) {
+        return authenticateUser(realm, "", "");
+    }
+
+    private RestResponseAuthentication authenticateUser(String realm, String username, String password) {
+        // @TODO Add support for custom authentication parameters
+
+        try {
+            securityManager.authenticateUser("", "", "");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        RestResponseAuthentication response = new RestResponseAuthentication(sessionManager.generateToken(), "All is good");
+        response.addAttribute("custom-attribute-1", "custom-value-1");
+        response.addAttribute("realm", realm);
+        return response;
+    }
 }
